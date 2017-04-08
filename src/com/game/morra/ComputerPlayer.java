43d920afbcbf56144d6a9719f46da9bed7ec2785
Lorenzo Cipriani <lorenzo1974@gmail.com>
2017-04-08 14:07:54 +0100

    Update ComputerPlayer.java

--- src/com/game/morra/ComputerPlayer.java
+++ src/com/game/morra/ComputerPlayer.java
@@ -2,9 +2,6 @@ package com.game.players;
 
 import java.util.ArrayList;
 
-/**
- * Created by alanpanno on 22/11/2015.
- */
 public class ComputerPlayer extends Player {
 
     public ComputerPlayer() {
