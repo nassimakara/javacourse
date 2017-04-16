ef38a3704925ebbccc5c356302381774c9f8bb29
ozultd <ozultd@hotmail.com>
2017-04-16 19:47:27 +0100

    human player

---
+++ doc/uml-seb/Human.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+public class Human {
+	
+	
+	
+	 public void setOddsorEven(boolean isOddsorEven) {
+	        this.isOddsorEven = isOddsorEven;
+	    }
+	 
+	 public void setFingers(int fingers) {
+	        this.fingers = fingers;
+	    }
+	 
+	 public int getFingers(int fingers) {
+	        return fingers;
+	    }
+	 
+	 
+	
+}
