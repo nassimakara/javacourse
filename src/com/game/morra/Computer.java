a08c63a7a78fb6c83beacd53ad9b85b7ae056ff2
lorenzocipriani <lorenzo1974@gmail.com>
2017-04-21 23:14:54 +0100

    Code reviewed, TODO added

--- src/com/game/morra/Computer.java
+++ src/com/game/morra/Computer.java
@@ -1,9 +1,14 @@
 package com.game.morra;
 
+// TODO: the code MUST be FORMATTED properly (indentation, spaces, etc...)
+
+// TODO: this class needs a description and there are several errors, e.g.:
+// -  the class that it extends is named Player and not Players
 public class Computer extends Players{
 	
-
+	// TODO: The name of this method is wrong and it should be getFingers() or showFingers() for both Computer and Human class 
 	public int generateRandomFingers() {
+		// TODO what is fingers? There is a missing type declaration
         fingers = (int) (Math.random() * 10);
         return fingers; 
 }
